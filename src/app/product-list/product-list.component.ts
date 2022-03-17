6b01425f8646ed6d3421a988ed9870309270dd3c
pallavi.gupta <pallavi.gupta@kochartech.com>
2022-03-17 13:17:30 +0530

    feat : notify me

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,4 +10,7 @@ export class ProductListComponent {
   share(){
     window.alert('This product has been shared')
   }
+  notifyMe() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
